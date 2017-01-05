52770413bf2be0294e8aadb76cb54605a59ed5d8
udunig <udunig@hotmail.com>
2017-01-05 12:04:46 +0100

    header for both platform is being done

--- app/index.js
+++ app/index.js
@@ -1,22 +1,20 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import { Router, Scene, Actions } from 'react-native-router-flux';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Store, BaseURl } from './config';
+import { Store } from './config';
 import Info from './routes/info';
 import News from './routes/news';
 import Collect from './routes/collect';
 import Photo from './routes/photo';
+import Theme from './styles';
+import I18n from './config/lang/i18.js';
 
 console.disableYellowBox = true;
-
-class nav extends Component {
-  render() {
-    return <Text>ilyas</Text>;
-  }
-};
+const { width } = Dimensions.get('window');
 
 class App extends Component {
 
@@ -25,36 +23,75 @@ class App extends Component {
   render() {
     const TabIcon = ({ selected, title }) => {
       return (
-        <Text style={{color: selected ? 'red' : 'black'}}>{title}</Text>
+          <View>
+            <Text style={{color: selected ? 'red' : 'black'}}>{title}</Text>
+          </View>
       );
     };
 
-    const navBar = () => {
-      return <Text>ilyas</Text>;
+    const navBarLeftBtn = () => {
+      return (
+        <View style={styles.leftBtnHolder}>
+          <Text style={{ color: Platform.ios ? '#0A0A0A' : '' }}>{I18n.t('header.buttonText')}</Text>
+          <Icon name="arrow-drop-down" size={38} color={Platform.ios ? '#0A0A0A' : ''} />
+        </View>);
     };
+
+    const navBarRightBtn = () => (
+      <View>
+        <Text>right</Text>
+      </View>
+    );
 
     return (
       <Provider store={this.store}>
-        <Router renderBackButton={navBar}>
-          <Scene key="root" rightTitle="Apply">
+        <Router
+          renderLeftButton={navBarLeftBtn}
+          renderRightButton={navBarRightBtn}
+          navigationBarStyle={{backgroundColor: Theme.navBarBGColor}}
+        >
+          <Scene key="root">
             {/* Tab Container */}
             <Scene
               key="tabbar"
               tabs
-              tabBarStyle={{ backgroundColor: '#FFFFFF' }}
+              tabBarStyle={{ backgroundColor: Theme.barBarBGColor }}
             >
               {/* Tab and it's scenes */}
-              <Scene key="Info" title='Info' hideNavBar rightTitle="Apply" component={Info} icon={TabIcon} />
+              <Scene key="Info" title="Info" icon={TabIcon}>
+                <Scene
+                  key="InfoMain"
+                  component={Info}
+                  title=""
+                />
+              </Scene>
 
               {/* Tab and it's scenes */}
-              <Scene key="News" title="News" component={News} icon={TabIcon} />
+              <Scene key="News" title="News" icon={TabIcon}>
+                <Scene
+                  key="NewsMain"
+                  component={News}
+                  title=""
+                />
+              </Scene>
 
               {/* Tab and it's scenes */}
-              <Scene key="Collect" title="Collect" component={Collect} icon={TabIcon} />
+              <Scene key="Collect" title="Collect" icon={TabIcon}>
+                <Scene
+                  key="CollectMain"
+                  component={Collect}
+                  title=""
+                />
+              </Scene>
 
               {/* Tab and it's scenes */}
-              <Scene key="Photo" title="Photo" component={Photo} icon={TabIcon} />
-
+              <Scene key="Photo" title="Photo" icon={TabIcon}>
+                <Scene
+                  key="PhotoMain"
+                  component={Photo}
+                  title=""
+                />
+              </Scene>
             </Scene>
           </Scene>
         </Router>
@@ -62,4 +99,14 @@ class App extends Component {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  leftBtnHolder: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: width * 0.8
+  }
+
+});
+
 export default App;
--- app/styles/index.js
+++ app/styles/index.js
@@ -1,5 +1,8 @@
-const Theme = {
+import { Platform } from 'react-native';
 
+const Theme = {
+  navBarBGColor: '#4CAF50',
+  barBarBGColor: '#5067FF'
 };
 
 export default Theme;
